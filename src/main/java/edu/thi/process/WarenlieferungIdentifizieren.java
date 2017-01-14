765da2776045c6f17aa815cefa64009447fa2e7f
chm2251 <chm2251@thi.de>
2017-01-14 23:23:01 +0100

    changed messages

--- src/main/java/edu/thi/process/WarenlieferungIdentifizieren.java
+++ src/main/java/edu/thi/process/WarenlieferungIdentifizieren.java
@@ -1,13 +1,20 @@
 package edu.thi.process;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.runtime.Execution;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class WebServiceClient implements JavaDelegate {
+public class WarenlieferungIdentifizieren implements JavaDelegate {
 
+    @SuppressWarnings("unchecked")
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         Long bestellID = (Long) execution.getVariable("bestellID");
@@ -17,7 +24,7 @@ public class WebServiceClient implements JavaDelegate {
         WebResource webResource = client
            .resource("http://localhost:8080/Bestellungen");
         String input = "{\"bestellID\":\""+bestellID+"}";
-        ClientResponse response = webResource.type("application/json")
+        ClientResponse response = webResource.type("application/jso")
            .post(ClientResponse.class, input);
         if (response.getStatus() != 200) {
             throw new RuntimeException("Failed : HTTP error code : "
@@ -25,7 +32,25 @@ public class WebServiceClient implements JavaDelegate {
          }
 
          //Bestellungen bestellungen = response.getEntity(Bestellungen.class);
-         execution.getVariable("ack");
+         Map<String, Object> ack = (Map<String, Object>) execution.getVariable("ack");
+         
+         Map<String, Object> data = new HashMap<String, Object>();
+         data.put("ack", ack);
+         
+         RuntimeService runtimeService = execution.getEngineServices().getRuntimeService();
+         
+         // Search if a process is already waiting at an intermediate event
+         Execution waitingExecution = runtimeService.createExecutionQuery()
+                   .messageEventSubscriptionName("AnfrageToERP")
+                   .singleResult();
+
+         if (waitingExecution != null) {
+             // An execution is waiting --> continue it
+             runtimeService.messageEventReceived("AnfrageToERP", waitingExecution.getId(), ack);
+         } else {
+             // No execution is waiting --> start a new Aggregator instance
+             runtimeService.startProcessInstanceByMessage("AnfrageToERP", ack);
+         }
     }
 
 }
